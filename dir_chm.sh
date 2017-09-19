708207678b9098d304227ff8aa4001e0cb3cfe19
Yukio Shiiya <yuk@yuknet.jp>
2017-09-19 19:46:20 +0900

    Import from private Git repository.

---
+++ dir_chm.sh
@@ -0,0 +1,138 @@
+#!/bin/sh
+
+# ==============================================================================
+#   機能
+#     ディレクトリリストに記載されたディレクトリのオーナ・グループ・パーミッションを設定する
+#   構文
+#     USAGE 参照
+#
+#   Copyright (c) 2006-2017 Yukio Shiiya
+#
+#   This software is released under the MIT License.
+#   https://opensource.org/licenses/MIT
+# ==============================================================================
+
+######################################################################
+# 関数定義
+######################################################################
+PRE_PROCESS() {
+	:
+}
+
+POST_PROCESS() {
+	:
+}
+
+USAGE() {
+	cat <<- EOF 1>&2
+		Usage:
+		    dir_chm.sh [OPTIONS ...] DIR_LIST
+		
+		    DIR_LIST : Specify a directory list.
+		
+		OPTIONS:
+		    --help
+		       Display this help and exit.
+	EOF
+}
+
+CMD_V() {
+	(eval "set -x; $*")
+}
+
+######################################################################
+# 変数定義
+######################################################################
+LS_OPTIONS="-ald"
+
+######################################################################
+# メインルーチン
+######################################################################
+
+# オプションのチェック
+CMD_ARG="`getopt -o \"\" -l help -- \"$@\" 2>&1`"
+if [ $? -ne 0 ];then
+	echo "-E ${CMD_ARG}" 1>&2
+	USAGE;exit 1
+fi
+eval set -- "${CMD_ARG}"
+while true ; do
+	opt="$1"
+	case "${opt}" in
+	--help)
+		USAGE;exit 0
+		;;
+	--)
+		shift 1;break
+		;;
+	esac
+done
+
+# 第1引数のチェック
+if [ "$1" = "" ];then
+	echo "-E Missing DIR_LIST argument" 1>&2
+	USAGE;exit 1
+else
+	DIR_LIST="$1"
+	# ディレクトリリストのチェック
+	if [ ! -f "${DIR_LIST}" ];then
+		echo "-E DIR_LIST not a file -- \"${DIR_LIST}\"" 1>&2
+		USAGE;exit 1
+	fi
+fi
+
+# 作業開始前処理
+PRE_PROCESS
+
+#####################
+# メインループ 開始 #
+#####################
+
+while read line ; do
+	echo "${line}" | grep -q -e '^#' -e '^$'
+	if [ $? -ne 0 ];then
+		dir="`echo \"${line}\" | awk '{print $1}'`"
+		uname="`echo \"${line}\" | awk '{print $2}'`"
+		gname="`echo \"${line}\" | awk '{print $3}'`"
+		mod="`echo \"${line}\" | awk '{print $4}'`"
+		if [ "${dir}" = "" -o "${uname}" = "" -o "${gname}" = "" -o "${mod}" = "" ];then
+			echo "-E Invalid DIR_LIST format -- \"${DIR_LIST}\"" 1>&2
+			echo "${line}"
+			POST_PROCESS;exit 1
+		fi
+
+		# 既存ディレクトリの場合
+		if [ \( -d "${dir}" \) -a \( ! -h "${dir}" \) ];then
+			# ディレクトリのオーナ・グループ設定
+			CMD_V "chown ${uname}:${gname} \"${dir}\""
+			if [ $? -ne 0 ];then
+				echo "-E Command has ended unsuccessfully." 1>&2
+				POST_PROCESS;exit 1
+			fi
+			# ディレクトリのモード設定
+			CMD_V "chmod ${mod} \"${dir}\""
+			if [ $? -ne 0 ];then
+				echo "-E Command has ended unsuccessfully." 1>&2
+				POST_PROCESS;exit 1
+			fi
+			# ディレクトリのLL
+			eval "ls ${LS_OPTIONS} \"${dir}\""
+			if [ $? -ne 0 ];then
+				echo "-E Command has ended unsuccessfully." 1>&2
+				POST_PROCESS;exit 1
+			fi
+		# 既存ディレクトリでない場合
+		else
+			echo "-W \"${dir}\" directory not exist, aborted" 1>&2
+			POST_PROCESS;exit 1
+		fi
+	fi
+done < "${DIR_LIST}"
+
+#####################
+# メインループ 終了 #
+#####################
+
+# 作業終了後処理
+POST_PROCESS;exit 0
+
